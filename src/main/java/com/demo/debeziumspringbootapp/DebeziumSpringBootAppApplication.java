3692873c1eb102a87c18985bdcd663731ce3b920
almonny <115696001+almonny@users.noreply.github.com>
2022-10-13 11:31:42 +0300

    Create DebeziumSpringBootAppApplication.java

---
+++ src/main/java/com/demo/debeziumspringbootapp/DebeziumSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.debeziumspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumSpringBootAppApplication.class, args);
+	}
+
+}
